bd24644e24daf4e17ce5d848ca8df668aae0a0c8
Davide Fiorello <davide@codeflyer.com>
2022-11-20 11:41:38 +0100

    feat: add pagination params to film list

--- src/modules/film/resolvers.js
+++ src/modules/film/resolvers.js
@@ -2,8 +2,14 @@ const SQL = require('@nearform/sql')
 module.exports = {
   Query: {
     getFilmList: async (parent, params, context, info) => {
+      const limit = params.input.resultPerPage || 5
+      const offset = (params.input.page || 0) * limit
+
       const query = SQL`
-        SELECT * from film LIMIT 5
+        SELECT * from film 
+        ORDER BY film_id
+        LIMIT ${limit}
+        OFFSET ${offset}
       `
 
       const result = await context.app.pg.query(query)
