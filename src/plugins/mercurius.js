9cf12d3575ab78c2f53febad2bfaf0d57a5c0056
Davide Fiorello <davide@codeflyer.com>
2022-11-20 13:13:46 +0100

    feat: add validation

--- src/plugins/mercurius.js
+++ src/plugins/mercurius.js
@@ -3,16 +3,21 @@
 const path = require('path')
 const mercurius = require('mercurius')
 const fp = require('fastify-plugin')
+const { parse } = require('graphql')
 const {loadFiles} = require('@graphql-tools/load-files')
 const {makeExecutableSchema} = require('@graphql-tools/schema')
+const mercuriusAuth = require('mercurius-auth')
+const mercuriusValidation = require('mercurius-validation')
 
 const dynamicImport = async (packageName) =>
   new Function(`return import('${packageName}')`)() // eslint-disable-line no-new-func
 
 const loadSchema = async () => {
-  const schema = await loadFiles(
+  const schema = [
+    parse(mercuriusValidation.graphQLTypeDefs),
+    ...await loadFiles(
     path.resolve(__dirname, '../modules/**/*.graphql')
-  )
+  )]
 
   const resolvers = (await loadFiles(
     path.resolve(__dirname, '../modules/**/resolvers.js')
@@ -71,6 +76,19 @@ module.exports = fp(
 
     fastify.register(mercurius, mercuriusOptions)
 
+    fastify.register(mercuriusAuth, {
+      authContext (context) {
+        return {
+          identity: context.reply.request.headers['x-user']
+        }
+      },
+      async applyPolicy (authDirectiveAST, parent, args, context, info) {
+        return context.auth.identity === 'admin'
+      },
+      authDirective: 'auth'
+    })
+
+    fastify.register(mercuriusValidation)
     fastify.register(explain.default, {})
 
     fastify.get('/sdl', async function () {
